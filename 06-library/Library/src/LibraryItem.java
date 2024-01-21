7b4ee7e8c4daa96dedbf78dbb98fe7d3f395deb4
ebbmango <ebbmango@tuta.io>
2024-01-21 14:12:23 +0100

    added (auto-incremented) IDs to library items and users

--- 06-library/Library/src/LibraryItem.java
+++ 06-library/Library/src/LibraryItem.java
@@ -1,10 +1,31 @@
+import java.util.Collection;
+
 public abstract sealed class LibraryItem permits Book, Journal, Movie {
+    protected int id;
+    private static int nextId = 1;
+
+    LibraryItem() {
+        this.id = nextId++;
+    }
+
+    private int daysOverdue;
     private boolean isAvailable = true;
+
+//    private
+
     protected double dailyFee;
 
     public void show() {
         // meant to be overridden
     }
+
+//    public boolean isOverdue() {
+//
+//    }
+//
+//    public int daysOverdue() {
+//
+//    }
 
     public boolean isAvailable() {
         return isAvailable;
--- 06-library/Library/src/LibraryUser.java
+++ 06-library/Library/src/LibraryUser.java
@@ -1,11 +1,13 @@
 public abstract sealed class LibraryUser permits Student, FacultyMember {
     protected int id;
+    private static int nextId = 1;
     protected boolean isPunctual;
 
     protected int[] maxLoanLength; // books, journals, movies
     protected int[] maxLoanAmount; // books, journals, movies
 
     LibraryUser(boolean isPunctual) {
+        this.id = nextId++;
         this.isPunctual = isPunctual;
     }
 
