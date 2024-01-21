dd7ce8b4e37b629ae1bd0d168963c53db6a3d5e4
ebbmango <ebbmango@tuta.io>
2024-01-21 14:25:20 +0100

    created function for checking success or failure given probability

--- 06-library/Library/src/Utilities.java
+++ 06-library/Library/src/Utilities.java
@@ -2,6 +2,17 @@ import java.util.Random;
 
 public class Utilities {
 
+    public static boolean rollTheDice(double probability) {
+        if (probability < 0.0 || probability > 1.0) {
+            throw new IllegalArgumentException("Invalid input: Probability must be between 0.");
+        }
+
+        double randomValue = Math.random();
+        double probabilityThreshold = probability * 100;
+
+        return randomValue <= probabilityThreshold;
+    }
+
     public static void shuffleArray(boolean[][] array) {
         Random random = new Random();
         for (int i = array.length - 1; i > 0; i--) {
