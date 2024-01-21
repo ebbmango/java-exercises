86180b5da43827627fb5a2aec067fb670318864c
ebbmango <ebbmango@tuta.io>
2024-01-21 13:31:24 +0100

    implemented isAvailable method and attribute for LibraryItems

--- 06-library/Library/src/LibraryItem.java
+++ 06-library/Library/src/LibraryItem.java
@@ -1,9 +1,17 @@
 public abstract sealed class LibraryItem permits Book, Journal, Movie {
-
+    private boolean isAvailable = true;
     protected double dailyFee;
 
     public void show() {
         // meant to be overridden
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrow() {
+        isAvailable = false;
     }
 
     public double getDailyFee() {
